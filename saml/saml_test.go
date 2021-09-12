a2c9e9221590b6c53d1bce565318fb797c636ead
Lenon Marcel <lenon.marcel@gmail.com>
2021-09-12 15:58:18 -0300

    Replace suite by simple test functions

--- saml/saml_test.go
+++ saml/saml_test.go
@@ -4,64 +4,55 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 )
 
-type TestSuite struct {
-	suite.Suite
-	base64sample string
-}
-
-func (s *TestSuite) SetupTest() {
+func base64Sample(t *testing.T) string {
 	data, err := ioutil.ReadFile("testdata/saml-response.base64")
 	if err != nil {
-		s.T().Fatal(err)
+		t.Fatal(err)
 	}
 
-	s.base64sample = string(data)
+	return string(data)
 }
 
-func (s *TestSuite) TestDecodeError() {
+func TestDecodeError(t *testing.T) {
 	response, err := Decode("invalid base 64")
 
-	s.Error(err)
-	s.Contains(err.Error(), "illegal base64 data")
-	s.Nil(response)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "illegal base64 data")
+	assert.Nil(t, response)
 }
 
-func (s *TestSuite) TestDecodeSuccess() {
-	response, err := Decode(s.base64sample)
+func TestDecodeSuccess(t *testing.T) {
+	response, err := Decode(base64Sample(t))
 
-	s.Nil(err)
-	s.Len(response.Assertion.AttributeStatement.Attributes, 13)
+	assert.Nil(t, err)
+	assert.Len(t, response.Assertion.AttributeStatement.Attributes, 13)
 }
 
-func (s *TestSuite) TestGetAWSRoles() {
-	response, _ := Decode(s.base64sample)
+func TestGetAWSRoles(t *testing.T) {
+	response, _ := Decode(base64Sample(t))
 	roles, err := response.GetAWSRoles()
 
-	s.Nil(err)
-	s.Len(roles, 2)
+	assert.Nil(t, err)
+	assert.Len(t, roles, 2)
 }
 
-func (s *TestSuite) TestFindPrincipalAndRoleToAssume() {
-	response, _ := Decode(s.base64sample)
+func TestFindPrincipalAndRoleToAssume(t *testing.T) {
+	response, _ := Decode(base64Sample(t))
 	principal, role, err := response.FindPrincipalAndRoleToAssume("firstrole")
 
-	s.Nil(err)
-	s.Equal(principal, "arn:aws:iam::111111111111:saml-provider/Example")
-	s.Equal(role, "arn:aws:iam::111111111111:role/FirstRole")
+	assert.Nil(t, err)
+	assert.Equal(t, principal, "arn:aws:iam::111111111111:saml-provider/Example")
+	assert.Equal(t, role, "arn:aws:iam::111111111111:role/FirstRole")
 }
 
-func (s *TestSuite) TestFindPrincipalAndRoleToAssumeNotFound() {
-	response, _ := Decode(s.base64sample)
+func TestFindPrincipalAndRoleToAssumeNotFound(t *testing.T) {
+	response, _ := Decode(base64Sample(t))
 	principal, role, err := response.FindPrincipalAndRoleToAssume("other")
 
-	s.EqualError(err, "could not find AWS principal and role other")
-	s.Empty(principal)
-	s.Empty(role)
-}
-
-func TestSaml(t *testing.T) {
-	suite.Run(t, new(TestSuite))
+	assert.EqualError(t, err, "could not find AWS principal and role other")
+	assert.Empty(t, principal)
+	assert.Empty(t, role)
 }
