bab132b354b05f8f64e9d500ed8791754d3d08b6
Lenon Marcel <lenon.marcel@gmail.com>
2021-09-13 10:59:06 -0300

    Prepare version.go to receive version and commit as ldflags

--- cmd/version.go
+++ cmd/version.go
@@ -6,6 +6,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// replaced via ldflags in the release script
+var gitTag = "v0.0.0"
+var gitCommit = "none"
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
@@ -14,6 +18,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of aws-cas-credential-process and exit",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("aws-cas-credential-process v0.1")
+		fmt.Println(fmt.Sprintf("aws-cas-credential-process %s %s", gitTag, gitCommit))
 	},
 }
